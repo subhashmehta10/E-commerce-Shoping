edcac5b53b5a715319dc1868004303d888d5459b
Subhash Mehta <subhashkumar746310@gmail.com>
2026-01-21 21:49:19 +0530

    oriders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -103,7 +103,7 @@ const Orders = () => {
 
                 {/* Header & Search */}
                 <div className="orders-header">
-                    <h1 className="orders-title">Your Orders</h1>
+                    <h1 className="orders-title">Your  Orders</h1>
                     <div className="orders-search-box">
                         <svg className="search-icon-orders" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
                         <input
